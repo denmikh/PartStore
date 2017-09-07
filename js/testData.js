a6d5fcff3e64b1edbf8ff35bb344ce4cee0d464c
denmikh <denmih61@gmail.com>
2017-09-07 15:40:15 +0300

    New Project

---
+++ js/testData.js
@@ -0,0 +1,23 @@
+
+	var scrollobject = [];
+
+	scrollobject[0] ={
+		id: "1",
+    name: "object 1"
+	}
+	var person = {
+		firstName: 'Brad',
+		age: 45,
+		children: ['Liza', 'Petya'],
+		adress: {
+			street: '555 Some st',
+			city: 'Boston',
+			state: 'MA'
+		}}
+
+		$('#myModal').modal('toggle');
+
+		$('#myModal').on('Создание вкладок, табов (tabs.js).bs.modal', function (event){
+			//Функция
+			alert("Выполненно")
+		});
