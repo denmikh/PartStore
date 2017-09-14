a1142acde0e38dda7fa1203844cfadcbbafa4437
denmikh <denmih61@gmail.com>
2017-09-14 17:59:22 +0300

    jQeury(New articles), styles

--- js/testData.js
+++ js/testData.js
@@ -1,23 +1,38 @@
 
-	var scrollobject = [];
+	var partsbase = [];
 
-	scrollobject[0] ={
+	partsbase[0] ={
 		id: "1",
-    name: "object 1"
-	}
-	var person = {
-		firstName: 'Brad',
-		age: 45,
-		children: ['Liza', 'Petya'],
-		adress: {
-			street: '555 Some st',
-			city: 'Boston',
-			state: 'MA'
-		}}
+    name: "SACHS 34542",
+		description: "Серия амортизаторов SUPER TOURING",
+		applicability: "Nissan Almera N16, Nissan Primera P12",
+		price: "4354 руб.",
+		url_img: "img/1.jpg"
+	};
 
-		$('#myModal').modal('toggle');
+	partsbase[1] ={
+		id: "2",
+    name: "Nissan 54320-95F0A",
+		description: "Оригинальная опора.",
+		applicability: "Nissan Almera N16, Nissan Primera P12",
+		price: "1350 руб.",
+		url_img: "img/2.jpg"
+	};
 
-		$('#myModal').on('Создание вкладок, табов (tabs.js).bs.modal', function (event){
-			//Функция
-			alert("Выполненно")
-		});
+	partsbase[2] ={
+		id: "3",
+		name: " Bridgestone Turanza T001",
+		description: "Премиальная летняя шина.",
+		applicability: "195/65 R15 91V",
+		price: "3654 руб.",
+		url_img: "img/3.jpg"
+	};
+
+	partsbase[3] ={
+		id: "4",
+		name: " Hyundai 28113-1R100",
+		description: "Фильтр воздушный.",
+		applicability: "Hyundai Solaris",
+		price: "200 руб.",
+		url_img: "img/4.jpg"
+	};
