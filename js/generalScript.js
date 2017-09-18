2c2e304044506e94f61f6f84ada34a169a01ef20
denmikh <denmih61@gmail.com>
2017-09-18 13:54:42 +0300

    модальное окно

--- js/generalScript.js
+++ js/generalScript.js
@@ -5,15 +5,16 @@
 
 function addNodes() {
   partsbase.forEach(function(item) {
-    var article = "<article>" + "<h2>" +item.name+ "<h2>" + "<img src='" +item.url_img+"'/>" + "<p>" +item.description+ "</p>" + "<p>"+item.applicability+"</p>" + "<h>"+item.price+"</h>" + "</article>"
+    var article = "<article id='"+item.id+"'>" + "<h2>" +item.name+ "<h2>" + "<img src='" +item.url_img+"'/>" + "<p>" +item.description+ "</p>" + "<p>"+item.applicability+"</p>" + "<h>"+item.price+"</h>" + "</article>"
     $(".articles").append(article);
-      //$("article").append(item.name +" "+ item.description +" "+ item.applicability +" "+ item.price +" "+ item.url_img);
 	});
 };
 
 function click() {
-$("article").bind("click", function (ev) {
-  console.log(ev);
-  alert("Привет");
+$("article").bind("click", function (event) {
+  console.log(event);
+  var data = event.currentTarget.innerHTML;
+  $("#text").html(data);
+  $("#myModal").modal('show');
 });
 };
