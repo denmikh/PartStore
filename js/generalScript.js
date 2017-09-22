685ede478d1039e738d7a6eb4f2c61ecfa76f33e
denmikh <denmih61@gmail.com>
2017-09-22 17:10:26 +0300

    complete modal

--- js/generalScript.js
+++ js/generalScript.js
@@ -10,11 +10,29 @@ function addNodes() {
 	});
 };
 
+
 function click() {
 $("article").bind("click", function (event) {
   console.log(event);
-  var data = event.currentTarget.innerHTML;
-  $("#text").html(data);
+  var id = parseInt(event.currentTarget.id);
+  var item = getDetail(id);
+  $("#myModal").find("#head").html(item.name);
+  $("#myModal").find("#min_description").html(item.description);
+  $("#myModal").find("#img").html("<img src='" +item.url_img+"'/>");
+  $("#myModal").find("#price").html(item.price);
+  $("#myModal").find("#article").html(item.orig_article);
+  $("#myModal").find("#applicability").html(item.applicability);
+  $("#myModal").find("#full_description").html(item.full_description);
   $("#myModal").modal('show');
 });
+};
+
+function getDetail(id) {
+  var result;
+  partsbase.forEach(function(item) {
+    if (id == item.id) {
+      result = item;
+    }
+  });
+  return result;
 };
--- js/testData.js
+++ js/testData.js
@@ -2,26 +2,31 @@
 	var partsbase = [];
 
 	partsbase[0] ={
-		id: "1",
+		id: 1,
     name: "SACHS 34542",
+		orig_article: "Nissan 54303-BN425",
 		description: "Серия амортизаторов SUPER TOURING",
+		full_description: "Амортизаторы на любой вкус и по сходной цене отвечают высоким требованиям комфорта и безопасности – почти для любого автомобиля. Максимальная надежность амортизаторов Super Touring устанавливает стандарт для этой отрасли. Разработанные и созданные со всей компетенцией завода-изготовителя они представляют собой безопасность, надежность и стабильность.",
 		applicability: "Nissan Almera N16, Nissan Primera P12",
 		price: "4354 руб.",
 		url_img: "img/1.jpg"
 	};
 
 	partsbase[1] ={
-		id: "2",
+		id: 2,
     name: "Nissan 54320-95F0A",
+		orig_article: "Nissan 54320-95F0A",
 		description: "Оригинальная опора.",
+		full_description: "Оригинальная опора Nissan. Качество 100%",
 		applicability: "Nissan Almera N16, Nissan Primera P12",
 		price: "1350 руб.",
 		url_img: "img/2.jpg"
 	};
 
 	partsbase[2] ={
-		id: "3",
+		id: 3,
 		name: " Bridgestone Turanza T001",
+		orig_article: "T001",
 		description: "Премиальная летняя шина.",
 		applicability: "195/65 R15 91V",
 		price: "3654 руб.",
@@ -29,8 +34,9 @@
 	};
 
 	partsbase[3] ={
-		id: "4",
-		name: " Hyundai 28113-1R100",
+		id: 4,
+		name: "Hyundai 28113-1R100",
+		orig_article: "Hyundai 28113-1R100",
 		description: "Фильтр воздушный.",
 		applicability: "Hyundai Solaris",
 		price: "200 руб.",
