e3e1c47c62ef583c0033830caf4b5c5e059a94d8
denmikh <denmih61@gmail.com>
2017-12-05 17:37:59 +0300

    new

--- partscheck.php
+++ partscheck.php
@@ -14,3 +14,18 @@
         ]"
     );
 ?>
+<!-- 
+<script>
+$(document).ready (function () {
+    $("select[name='country']").bind("change", function () {
+        $("select[name='city']").empty();
+        $.get("countrycheck.php", {country: $("select[name='country']").val()}, function (data) {
+            data = JSON.parse(data);
+            $("select[name='city']").empty();
+            for(var id in data) {
+                $("select[name='city']").append($("<option value='"+ id +"'>" + data[id] + "</option>"));
+            }
+    });
+});
+});
+</script> -->
